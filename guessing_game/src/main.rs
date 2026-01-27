7883d85caccee5bef08969516b0cd93aad28b3cc
enigma-137 <onojaemmanuel953@gmail.com>
2026-01-27 15:48:58 +0100

    updated game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,39 +1,36 @@
 use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use rand::Rng;
 
+fn main(){
+    println!("Guessing game");
 
-fn main() {
-    println!("Guess a number");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    //println!("The secret number is: {secret_number}");
-    loop {
+    loop{
+        println!("Please input a number");
 
-    println!("Enter a number to guess");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin().
+        read_line(& mut guess)
+        .expect("Failed to read line");
 
-    io::stdin()
-    .read_line(& mut guess)
-    .expect("Failed to read line");
+    let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        
+    };
 
-    // let guess: u32 = guess.trim().parse()
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    }; 
-    
-    //.expect("Please enter a number");
-println!("Your guess is: {guess}");
-
- match guess.cmp(&secret_number)
-{
-    Ordering::Less => println!("Too low"),
-    Ordering::Greater => println!("Too big"),
-    Ordering::Equal => {
-        println!("You Got it");
-        break;
+    match guess.cmp(& secret_number){
+        Ordering::Greater => print!("Too high"),
+        Ordering::Less => print!("Too low"),
+        Ordering::Equal => {
+            print!("You got it!");
+            break;
         }
+    }
+    
+
+    }
 }
-}}
