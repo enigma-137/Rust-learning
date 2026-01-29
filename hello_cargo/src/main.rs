a359571714c1c93bca43764b7a3f1a12f6f84ac5
enigma-137 <onojaemmanuel953@gmail.com>
2026-01-29 06:23:26 +0100

    array

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an index");
+
+    let mut index = String::new();
+
+    io::stdin()
+    .read_line(& mut index)
+    .expect("This is not what we expected");
+
+    let index: usize = index.trim().parse().expect("Number too not so okay");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
