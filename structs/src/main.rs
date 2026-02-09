01751e466b2492a3da72ca35f27266227bd15293
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-09 08:14:41 +0100

    structs lesson

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,39 +1,52 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+// struct User {
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+//     active: bool,
+// }
+
+fn area(width:u32, height:u32) -> u32{
+    width * height
 }
 
 fn main() {
-    let mut user1 = User{
-        email: String::from("shola@gmail.com"),
-        username: String::from("Sholexy"),
-        sign_in_count: 1,
-        active: true
-    };
+
+
+    let width = 70;
+    let height = 50;
+
+    println!("The area of the rectangle is {} square pixels", area(width, height));
+    // let mut user1 = User{
+    //     email: String::from("shola@gmail.com"),
+    //     username: String::from("Sholexy"),
+    //     sign_in_count: 1,
+    //     active: true
+    // };
 
     // println!("The email is {}", user1.email)
-    user1.username = String::from("Jose sholly");
-    let name = user1.username;
-    println!("The name is {}", name);
+//     user1.username = String::from("Jose sholly");
+//     let name = user1.username;
+//     println!("The name is {}", name);
 
-    let user2 = build_user(String::from("toyo@gmail.com"), String::from("t4_toyo"));
-    println!("The second user is {}", user2.username);
+//     let user2 = build_user(String::from("toyo@gmail.com"), String::from("t4_toyo"));
+//     println!("The second user is {}", user2.username);
 
-    let user3 = User{
-          email: String::from("Esther@gmail.com"),
-        username: String::from("Esther"),
-        ..user2
-    }
+//     let user3 = User{
+//           email: String::from("Esther@gmail.com"),
+//         username: String::from("Esther"),
+//         ..user2
+//     }
+// }
+
+
+// fn build_user(email: String, username: String) -> User{
+//    User {
+//      email, 
+//     username,
+//     active: true,
+//     sign_in_count: 1
+//    }
+
+
 }
 
-
-fn build_user(email: String, username: String) -> User{
-   User {
-     email, 
-    username,
-    active: true,
-    sign_in_count: 1
-   }
-}
