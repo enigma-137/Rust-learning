517f949ceded53603d497be5d38b81661ceac58f
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-10 16:16:39 +0100

    More structs lessons

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,17 +5,34 @@
 //     active: bool,
 // }
 
-fn area(width:u32, height:u32) -> u32{
-    width * height
+// fn area(dimensions: (u32, u32)) -> u32{
+//     dimensions.0 * dimensions.1
+// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+impl Rectangle {
+fn calc(&self) -> u32{
+    self.width * self.height
+}
 }
 
 fn main() {
 
 
-    let width = 90;
-    let height = 50;
+    let rect = Rectangle {
+        width: 32,
+        height:40
 
-    println!("The area of the rectangle is {} square pixels", area(width, height));
+    };
+
+    println!("Rect: {:#?} ", rect);
+
+
+    
+    println!("The area of the rectangle is {} square pixels", rect.calc() );
     // let mut user1 = User{
     //     email: String::from("shola@gmail.com"),
     //     username: String::from("Sholexy"),
