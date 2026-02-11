7bea4360632e7590e623bbd71b433e158a711519
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-11 18:31:39 +0100

    enums

--- enumss/src/main.rs
+++ enumss/src/main.rs
@@ -1,10 +1,23 @@
-enum IpAddress {
+enum IpAddressKind {
 v4,
 v6,
-
 }
 
-
+struct IpAddress {
+    kind: IpAddressKind,
+    address: String
+}
 fn main() {
-    println!("Hello, world!");
+    // specify variances
+    let four = IpAddressKind::v4;
+    let five = IpAddressKind::v6;
+// create the variable for the local host ip address
+    let localhost = IpAddress {
+        kind: IpAddressKind::v4,
+        address: String::from("127.0.0.1"),
+    };
+}
+
+fn route(ip_kind: IpAddressKind){
+
 }
