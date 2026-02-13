ab46281a5d82d6b41a1d22dabfabb92f1cb4c5d9
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-13 06:21:25 +0100

    lib functions

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// mod front_of_house {
+//    pub mod hosting {  //added pub to make it private
+//       pub  fn add_to_waitlist(){}
+//     }
+// }
+
+
+// pb fn eat_at_restaurant(){
+//     crate::front_of_house::hosting::add_to_waitlist();
+
+    //relative path
+
+
+// }
+
+// fn serve_order() {}
+
+// mod back_of_house {
+//     fn fix_incorrect_order(){
+//         cook_order();
+//         super::serve_order() //instead of adding pub, just use super to access parents fn
+
+//     }
+// }
+
+mod back_of_house(){
+    struct Breakfast {
+        toast: String,
+        seasonal_fruit: String,
+    }
+
+    //mod of house >> impl Breakfast >> summer function that resturns the breakfast values
+    impl Breakfast {
+   pub fn summer(toast: &str) -> Breakfast { //associated functiom with pub to make it public
+            Breakfast{
+            toast: String::from(toast),
+            seasonal_fruit: String::from("peaches"),
+            }
+           
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn eat_at_restaurant(){
+    let mut meal = back_of_house::Breakfast::summer()
 }
