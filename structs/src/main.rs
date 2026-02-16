8209ab34811008195f00d50e0a224c4d9c9da39f
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-16 06:19:03 +0100

    hmmmmmm

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,44 +1,18 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32
+struct MyStruct {
+    num: i32,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32{
-        self.height * self.width
-    }
-
-    fn can_hold(&self, other: &Rectangle)-> bool{
-        self.width > other.width && self.height > other.height
-    }
+fn print_struct(structs: &MyStruct){
+println!("{:#?}", structs);
 }
 
 fn main(){
-
-
-    let rect = Rectangle{
-        width: 32,
-        height: 30
-    };
-
-      let rect2 = Rectangle{
-        width: 20,
-        height: 24
-    };
-
-      let rect3 = Rectangle{
-        width: 42,
-        height: 50
-    };
-
-    println!("Rect: {:#?}", rect);
-
-    println!("The area is: {}", rect.area());
-
-    println!("This can hold: {}", rect.can_hold(&rect2));
-    println!("This can hold: {}", rect.can_hold(&rect3));
+    let my_structs: MyStruct = MyStruct{ num: 3 };
+    print_struct(&my_structs);
+    print_struct(&my_structs);
 
 }
+
 
 
