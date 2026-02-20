455bb1fcb33c588f0a7b6f0e701871663f3d368a
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-20 05:12:34 +0100

    target edit

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+//    let v:Vec<u32> = Vec::new();
+
+   let v2 = vec![1,2,3,4,5];
+
+//    match v2.get(30) {
+//     Some(third) => println!("The third value is {}", third),
+//     None => println!("There is no third value")
+//    }
+
+for i:i32 in &v2{
+    println!("{}", i);
+}
 }
