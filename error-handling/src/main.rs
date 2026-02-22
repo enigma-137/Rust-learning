b33d8ade134f274bfae6ed176c8dce90df4a964b
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-22 08:39:27 +0100

    rhghgh

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating file {:?}", e),
+            },
+            other_error => {
+                panic!("Error creating file {:?}", other_error);
+            }
+        }
+    };
+    // b();
 }
+// fn b(){
+//     c(22)
+// }
+
+// fn c(num: i32){
+//     if num == 22 {
+//         panic!("Don't pass 22 please")
+//     } 
+// }
