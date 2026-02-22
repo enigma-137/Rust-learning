e87c6b84b4fad21122b0bc264a55f443383b2369
enigma-137 <onojaemmanuel953@gmail.com>
2026-02-22 07:31:01 +0100

    error handling

--- Rust-todo/rust/src/main.rs
+++ Rust-todo/rust/src/main.rs
@@ -6,6 +6,38 @@ use std::sync::Mutex;
 use chrono::{Utc, DateTime};
 
 
-fn main(){
-    println!("Hello, world!");
+#[derive(Serialize, Clone, Deserialize)]
+struct TodoItem {
+    id: Uuid,
+    title: String,
+    description: String,
+    completed: bool,
+    created_at: DateTime<Utc>,
 }
+
+#[derive(Deserialize)]
+struct CreateTodoItem {
+    title: String,
+    description: String,
+}
+
+#[derive(Deserialize)]
+struct UpdateTodoItem {
+    title: Option<String>,
+    completed: Option<bool>,
+}
+
+struct AppState {
+    todo_list: Mutex<Vec<TodoItem>>,
+}
+
+async fn get_todos(data: web::Data<AppState>) -> impl Responder {
+    let todos = data.todo_list.lock().unwrap();
+    HttpResponse::ok().json(&*todos)
+}
+
+asyn fn add_todo(
+    item: web::Json::<CreateTodoItem>,
+    data: web::Data<AppState>,
+
+)
