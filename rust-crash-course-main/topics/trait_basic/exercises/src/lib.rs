8b1cd24d59f0fe523a99447abcaf20d0e3f9f100
enigma-137 <onojaemmanuel953@gmail.com>
2026-03-26 18:48:30 +0100

    a lot

--- rust-crash-course-main/topics/trait_basic/exercises/src/lib.rs
+++ rust-crash-course-main/topics/trait_basic/exercises/src/lib.rs
@@ -10,6 +10,19 @@ pub struct Cargo {
     pub version: String,
 }
 
-pub fn test(tester: ?, file_path: &str) -> String {
-    todo!();
+impl Tester for Foundry {
+    fn test(&self, file_path: &str) -> String {
+        format!("Test for foundry: {}", file_path)
+    }
+}
+
+impl Tester for Cargo {
+    fn test(&self, file_path: &str) -> String {
+        format!("Test for cargo: {}", file_path)
+    }
+}
+
+pub fn test(tester: &impl Tester, file_path: &str) -> String {
+    tester.test(file_path)
+    
 }
